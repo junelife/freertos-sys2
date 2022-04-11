e314b7e656979c1f280a39b3e70443b28c2d3faa
Cody Schafer <cody@junelife.com>
2022-04-11 18:17:46 -0400

    xSemaphore and xQueueGeneric functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,11 @@
 //! selection we needed. Additionally, the presence of some of these functions vary based on
 //! FreeRTOS configuration, as does the types used.
 #![no_std]
-// The types here should match the naming used in FreeRTOS
-#![allow(non_camel_case_types)]
+// The types, values, functions, and other items should match the naming used in FreeRTOS
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+#![forbid(unsafe_op_in_unsafe_fn)]
+
+use core::ffi::c_void;
 
 extern "C" {
     pub fn xTaskGetTickCountFromISR() -> TickType_t;
@@ -21,6 +24,31 @@ extern "C" {
         xTimeIncrement: TickType_t,
     ) -> BaseType_t;
     pub fn vTaskDelay(xTicksToDelay: TickType_t);
+
+    pub fn pvTaskGetThreadLocalStoragePointer(
+        xTaskToQuery: TaskHandle_t,
+        xIndex: BaseType_t,
+    ) -> *mut c_void;
+
+    pub fn vTaskSetThreadLocalStoragePointer(
+        xTaskToSet: TaskHandle_t,
+        xIndex: BaseType_t,
+        pvValue: *mut c_void,
+    );
+
+    pub fn xQueueGenericReceive(
+        xQueue: QueueHandle_t,
+        pvBuffer: *const c_void,
+        xTicksToWait: TickType_t,
+        xJustPeek: BaseType_t,
+    ) -> BaseType_t;
+
+    pub fn xQueueGenericSend(
+        xQueue: QueueHandle_t,
+        pvItemToQueue: *const c_void,
+        xTicksToWait: TickType_t,
+        xCopyPosition: BaseType_t,
+    ) -> BaseType_t;
 }
 
 pub type BaseType_t = i32;
@@ -30,4 +58,33 @@ pub type BaseType_t = i32;
 // adjust this.
 pub type TickType_t = u32;
 
-pub type TaskHandle_t = *mut core::ffi::c_void;
+pub type TaskHandle_t = *mut c_void;
+pub type QueueHandle_t = *mut c_void;
+pub type SemaphoreHandle_t = *mut c_void;
+
+// NOTE: items below here are defined as macros in freertos9, and should be scrutinized if one makes
+// changes to freertos
+
+pub const pdFALSE: BaseType_t = 0;
+pub const pdTRUE: BaseType_t = 1;
+
+pub const queueSEND_TO_BACK: BaseType_t = 0;
+pub const queueSEND_TO_FRONT: BaseType_t = 1;
+pub const queueOVERWRITE: BaseType_t = 2;
+
+pub const semGIVE_BLOCK_TIME: TickType_t = 0;
+
+pub unsafe fn xSemaphoreTake(xSemaphore: SemaphoreHandle_t, xBlockTime: TickType_t) {
+    unsafe { xQueueGenericReceive(xSemaphore, core::ptr::null(), xBlockTime, pdFALSE) };
+}
+
+pub unsafe fn xSemaphoreGive(xSemaphore: SemaphoreHandle_t) {
+    unsafe {
+        xQueueGenericSend(
+            xSemaphore,
+            core::ptr::null(),
+            semGIVE_BLOCK_TIME,
+            queueSEND_TO_BACK,
+        )
+    };
+}
